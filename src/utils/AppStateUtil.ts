1df79ea42248caeec9f103b6aff0ecccc58df2a4
Ameer Sami <ameersami@Ameers-MBP-2.fios-router.home>
2020-07-21 21:54:15 -0400

    Added abbility to save age and dob to locale storage. Also added favicon and apple home screen icons

--- src/utils/AppStateUtil.ts
+++ src/utils/AppStateUtil.ts
@@ -1,19 +1,21 @@
 type appActions = 'CHANGE_DOB' | 'CHANGE_DEATH_AGE';
 
 const appInitialState: { dob: Date, deathAge: number } = {
-  dob: new Date('1999-01-01'),
-  deathAge: 80
+  dob: localStorage.getItem('ded_app_dob') ? new Date(localStorage.getItem('ded_app_dob')) : new Date('1999-01-01'),
+  deathAge: localStorage.getItem('ded_app_deathAge') ? Number(localStorage.getItem('ded_app_deathAge')) : 80
 };
 
 const appStateReducer: any = (state, action: { type: appActions, payload: any }) => {
   const payload = action.payload;
   switch (action.type) {
     case 'CHANGE_DEATH_AGE':
+      localStorage.setItem('ded_app_deathAge', payload);
       return {
         ...state,
         deathAge: payload
       }
-    case 'CHANGE_DOB':
+      case 'CHANGE_DOB':
+      localStorage.setItem('ded_app_dob', payload);
       return {
         ...state,
         dob: payload
